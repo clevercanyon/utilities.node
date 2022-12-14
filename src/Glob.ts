0e29d1b387fccacb92edd85da5c52e2da9cbf7d5
Jason Caldwell <jaswrks@o5p.me>
2022-12-13 22:09:22 -0500

    Update.

---
+++ src/Glob.ts
@@ -0,0 +1,52 @@
+/**
+ * Utility class.
+ */
+
+import type { Options as GlobbyOptions } from 'globby';
+import { globby, globbyStream } from 'globby';
+
+/**
+ * Glob options.
+ */
+interface GlobOptions extends GlobbyOptions {}
+
+/**
+ * Default glob options.
+ */
+const defaultOptions: GlobOptions = {
+	expandDirectories: false,
+	absolute: true,
+};
+
+/**
+ * Glob utilities.
+ */
+export default class Glob {
+	/**
+	 * Globby promise.
+	 *
+	 * @param   patterns Glob pattern(s).
+	 * @param   options  Globby options.
+	 *
+	 * @returns          Promise with array of strings.
+	 *
+	 * @see https://github.com/sindresorhus/globby#globbystreampatterns-options
+	 */
+	public static async promise(patterns: string | string[], options: GlobOptions = defaultOptions): Promise<string[]> {
+		return globby(patterns, options);
+	}
+
+	/**
+	 * Globby stream.
+	 *
+	 * @param   patterns Glob pattern(s).
+	 * @param   options  Globby options.
+	 *
+	 * @returns          Readable stream for use in `for await()`.
+	 *
+	 * @see https://github.com/sindresorhus/globby#globbystreampatterns-options
+	 */
+	public static stream(patterns: string | string[], options: GlobOptions = defaultOptions): NodeJS.ReadableStream {
+		return globbyStream(patterns, options);
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export default {};
+/**
+ * Utility exports.
+ */
+
+export { default as Glob } from './Glob';
