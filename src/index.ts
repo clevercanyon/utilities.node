116861f814ac7a103368dce49af3c964e74723ff
Jason Caldwell <jaswrks@o5p.me>
2023-03-20 09:29:40 -0400

    Project update. [p][robotic]

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@
 export * as $chalk from './chalk.js';
 export * as $cmd from './cmd.js';
 export * as $dotenv from './dotenv.js';
+export * as $fs from './fs.js';
 export * as $glob from './glob.js';
 export * as $path from './path.js';
 export * as $prettier from './prettier.js';
