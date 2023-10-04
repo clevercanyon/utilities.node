b28756b0fc8a0ea26819a57534c07b219e34a934
Jason Caldwell <jaswrks@o5p.me>
2023-10-04 17:29:04 -0400

    Project update. [p][robotic]

--- src/dotenv.ts
+++ src/dotenv.ts
@@ -4,7 +4,7 @@
 
 import './resources/init.ts';
 
-import { $obj, type $type } from '@clevercanyon/utilities';
+import { $obj, $to, type $type } from '@clevercanyon/utilities';
 import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 import dotenvVault from 'dotenv-vault';
@@ -13,7 +13,10 @@ import fs from 'node:fs';
 /**
  * Defines types.
  */
-export type ParseExpandOptions = { ignoreProcessEnv?: boolean };
+export type ParseExpandOptions = {
+    ignoreProcessEnv?: boolean;
+    preserveProcessEnv?: boolean;
+};
 
 /**
  * Exports dotenv utilities.
@@ -30,20 +33,39 @@ export const { expand } = dotenvExpand;
  * - `\$NAME` will escape into a literal `$NAME` rather than expand it.
  * - `${NAME:-default}` If `NAME` does not exist, expands into `default`.
  *
- * @param   file    Absolute path to any `.env` file.
+ * When passing multiple files, the order is important, as they will cascade, with later files overriding variables set
+ * by previous files. This works the same as Vite’s build system. Note that in Vite, they use a patched copy of
+ * dotenv-expand, and they have it configured not to ignore `process.env`, but instead, not to write to `process.env`.
+ * That works beautifully, so we mimic that behavior here via `preserveProcessEnv`, which defaults to `true`.
+ *
+ * @param   files   Absolute path to `.env` file(s).
  * @param   options Options (all optional); {@see ParseExpandOptions}.
  *
  * @returns         Parsed/expanded environment variables.
  *
  * @see https://o5p.me/gx9wqb
  */
-export const parseExpand = (file: string, options?: ParseExpandOptions): $type.Object => {
-    const opts = $obj.defaults({}, options || {}, { ignoreProcessEnv: true }) as Required<ParseExpandOptions>;
-    const props = fs.readFileSync(file).toString();
-    return (
-        dotenvExpand.expand({
-            parsed: dotenv.parse(props),
-            ignoreProcessEnv: opts.ignoreProcessEnv,
-        }).parsed || {}
+export const parseExpand = (files: string | string[], options?: ParseExpandOptions): $type.Object => {
+    let origProcessEnv: NodeJS.ProcessEnv | undefined; // Used to preserve `process.env`, when applicable.
+    const opts = $obj.defaults({}, options || {}, { ignoreProcessEnv: false, preserveProcessEnv: true }) as Required<ParseExpandOptions>;
+
+    if (!opts.ignoreProcessEnv && opts.preserveProcessEnv) {
+        origProcessEnv = { ...process.env };
+    }
+    const env = Object.fromEntries(
+        $to.array(files).flatMap((file) => {
+            if (!fs.existsSync(file)) return [];
+
+            return Object.entries(
+                expand({
+                    ignoreProcessEnv: opts.ignoreProcessEnv,
+                    parsed: parse(fs.readFileSync(file).toString()),
+                }).parsed || {},
+            );
+        }),
     );
+    if (!opts.ignoreProcessEnv && opts.preserveProcessEnv && origProcessEnv) {
+        process.env = origProcessEnv;
+    }
+    return env;
 };
