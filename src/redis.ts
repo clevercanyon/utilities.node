9a656b9175c3d09e69d2a39ea0a17d180d5d5b3b
Jason Caldwell <jaswrks@o5p.me>
2024-12-30 10:13:03 -0500

    Project update. [p][robotic]

--- src/redis.ts
+++ src/redis.ts
@@ -86,6 +86,7 @@ export const instance = $fn.memo(
             url: opts.restURL,
             token: opts.restToken,
 
+            enableAutoPipelining: true, // Enable; should not have issues with this on Node.
             responseEncoding: false, // Do not base64-encode response data, which adds network latency.
             automaticDeserialization: true, // Yes, use JSON to encode/decode HTTP request/response data.
             enableTelemetry: false, // Just for good measure. The environment variable is what’s important.
